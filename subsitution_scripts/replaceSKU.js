1012e92784f0f552d257ea5e96e65211cce605ff
Austin Slater <aslater@waldoch.com>
2025-07-18 11:23:04 -0500

    new function fieldChanged acts as a final check before the line is committed

--- subsitution_scripts/replaceSKU.js
+++ subsitution_scripts/replaceSKU.js
@@ -3,53 +3,92 @@
  * @NScriptType ClientScript
  * @NModuleScope Public
  */
-define(['N/currentRecord', 'N/log'], function(currentRecord, log) {
+define(['N/currentRecord', 'N/log'], function (currentRecord, log) {
 
-  function validateLine(context) {
-    var rec = currentRecord.get();
-    var sublistName = context.sublistId;
+    function validateLine(context) {
+        var rec = currentRecord.get();
+        var sublistName = context.sublistId;
 
-    if (sublistName === 'item') {
-      var itemId = rec.getCurrentSublistValue({ sublistId: 'item', fieldId: 'item' });
+        if (sublistName === 'item') {
+            var itemId = rec.getCurrentSublistValue({ sublistId: 'item', fieldId: 'item' });
 
-      if (itemId == '63374') {
-        log.debug('Replacing discontinued item', 'Replacing item 63374 with 24844');
+            if (itemId == '63374') {
+                log.debug('Replacing discontinued item', 'Replacing item 63374 with 67803');
 
-        // Replace item
-        rec.setCurrentSublistValue({
-          sublistId: 'item',
-          fieldId: 'item',
-          value: '24844'
-        });
+                // Replace item
+                rec.setCurrentSublistValue({
+                    sublistId: 'item',
+                    fieldId: 'item',
+                    value: '67803'
+                });
 
-        // Set new description (or blank to auto-populate from item record)
-        rec.setCurrentSublistValue({
-          sublistId: 'item',
-          fieldId: 'description',
-          // blank means it should pull from item record if configured
-          value: '' 
-        });
+                // Set new description (or blank to auto-populate from item record)
+                rec.setCurrentSublistValue({
+                    sublistId: 'item',
+                    fieldId: 'description',
+                    // blank means it should pull from item record if configured
+                    value: ''
+                });
 
-        // Clear rate so pricing can recalculate, or hardcode if necessary
-        rec.setCurrentSublistValue({
-          sublistId: 'item',
-          fieldId: 'rate',
-          value: ''
-        });
+                // Clear rate so pricing can recalculate, or hardcode if necessary
+                rec.setCurrentSublistValue({
+                    sublistId: 'item',
+                    fieldId: 'rate',
+                    value: ''
+                });
 
-        // Clear MPN
-        rec.setCurrentSublistValue({
-          sublistId: 'item',
-          fieldId: 'custitemcustom_mpn',
-          value: ''
-        });
-      }
+                // Clear MPN
+                rec.setCurrentSublistValue({
+                    sublistId: 'item',
+                    fieldId: 'custitemcustom_mpn',
+                    value: ''
+                });
+            }
+        }
+
+        return true;
     }
 
-    return true;
-  }
+    function fieldChanged(context) {
+        var rec = currentRecord.get();
+        var sublistName = context.sublistId;
+        var fieldId = context.fieldId;
 
-  return {
-    validateLine: validateLine
-  };
+        if (sublistName === 'item' && fieldId === 'item') {
+            var itemId = rec.getCurrentSublistValue({ sublistId: 'item', fieldId: 'item' });
+
+            if (itemId == '63374') {
+                log.debug('Item changed - replacing discontinued item early', 'Replacing item 63374 with 67803');
+
+                rec.setCurrentSublistValue({
+                    sublistId: 'item',
+                    fieldId: 'item',
+                    value: '67803'
+                });
+
+                rec.setCurrentSublistValue({
+                    sublistId: 'item',
+                    fieldId: 'description',
+                    value: ''
+                });
+
+                rec.setCurrentSublistValue({
+                    sublistId: 'item',
+                    fieldId: 'rate',
+                    value: ''
+                });
+
+                rec.setCurrentSublistValue({
+                    sublistId: 'item',
+                    fieldId: 'custitemcustom_mpn',
+                    value: ''
+                });
+            }
+        }
+    }
+
+    return {
+        validateLine: validateLine,
+        fieldChanged: fieldChanged
+    };
 });
