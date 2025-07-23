be09be1f95c80f06698155ad96e94e4b102b7560
Austin Slater <aslater@waldoch.com>
2025-07-23 12:42:26 -0500

    updated with additional debugging

--- subsitution_scripts/substitute.js
+++ subsitution_scripts/substitute.js
@@ -3,7 +3,7 @@
  * @NScriptType ClientScript
  * @NModuleScope SameAccount
  */
-define(['N/record', 'N/ui/dialog', 'N/search'], function (record, dialog, search) {
+define(['N/record', 'N/ui/dialog', 'N/search', 'N/log'], function (record, dialog, search, log) {
     function validateLine(context) {
         var salesOrder = context.currentRecord;
         var sublistId = context.sublistId;
@@ -14,53 +14,69 @@ define(['N/record', 'N/ui/dialog', 'N/search'], function (record, dialog, search
                 fieldId: 'item'
             });
 
-            // Search for substitute items
-            var substituteSearch = search.create({
-                type: 'itemsubstitute',
-                filters: [
-                    ['item', 'is', itemId],
-                    'AND',
-                    ['isinactive', 'is', false]
-                ],
-                columns: [
-                    'substituteitem',
-                    'custrecord_substitute_type'
-                ]
-            });
+            try {
+                log.debug({
+                    title: 'Checking Record Type',
+                    details: 'Attempting to use itemsubstitute record type'
+                });
 
-            var searchResult = substituteSearch.run().getRange({ start: 0, end: 1 });
+                // Search for substitute items
+                var substituteSearch = search.create({
+                    type: 'itemsubstitute',
+                    filters: [
+                        ['item', 'is', itemId],
+                        'AND',
+                        ['isinactive', 'is', false]
+                    ],
+                    columns: [
+                        'substituteitem',
+                        'custrecord_substitute_type'
+                    ]
+                });
 
-            if (searchResult.length > 0) {
-                var substituteItemId = searchResult[0].getValue('substituteitem');
-                var substituteType = searchResult[0].getValue('custrecord_substitute_type');
-                var substituteItemName = searchResult[0].getText('substituteitem');
+                var searchResult = substituteSearch.run().getRange({ start: 0, end: 1 });
 
-                if (substituteType === 'SUPERSEDED') {
-                    dialog.alert({
-                        title: 'Superseded Item',
-                        message: 'The part "' + substituteItemName + '" supersedes this part and is being replaced inside the sales order.'
-                    }).then(function () {
-                        // Automatically replace with superseded item
-                        salesOrder.setCurrentSublistValue({
-                            sublistId: 'item',
-                            fieldId: 'item',
-                            value: substituteItemId
-                        });
-                    });
-                } else if (substituteType === 'REPLACEMENT') {
-                    dialog.confirm({
-                        title: 'Replacement Item Available',
-                        message: 'The part "' + substituteItemName + '" can replace this part. Would you like to replace it?'
-                    }).then(function (confirmed) {
-                        if (confirmed) {
+                if (searchResult.length > 0) {
+                    var substituteItemId = searchResult[0].getValue('substituteitem');
+                    var substituteType = searchResult[0].getValue('custrecord_substitute_type');
+                    var substituteItemName = searchResult[0].getText('substituteitem');
+
+                    if (substituteType === 'SUPERSEDED') {
+                        dialog.alert({
+                            title: 'Superseded Item',
+                            message: 'The part "' + substituteItemName + '" supersedes this part and is being replaced inside the sales order.'
+                        }).then(function () {
+                            // Automatically replace with superseded item
                             salesOrder.setCurrentSublistValue({
                                 sublistId: 'item',
                                 fieldId: 'item',
                                 value: substituteItemId
                             });
-                        }
-                    });
+                        });
+                    } else if (substituteType === 'REPLACEMENT') {
+                        dialog.confirm({
+                            title: 'Replacement Item Available',
+                            message: 'The part "' + substituteItemName + '" can replace this part. Would you like to replace it?'
+                        }).then(function (confirmed) {
+                            if (confirmed) {
+                                salesOrder.setCurrentSublistValue({
+                                    sublistId: 'item',
+                                    fieldId: 'item',
+                                    value: substituteItemId
+                                });
+                            }
+                        });
+                    }
                 }
+            } catch (e) {
+                log.error({
+                    title: 'Error in Item Substitute Search',
+                    details: 'Error searching itemsubstitute record: ' + e.message
+                });
+                dialog.alert({
+                    title: 'Error',
+                    message: 'An error occurred while checking for substitute items: ' + e.message
+                });
             }
         }
         return true;
